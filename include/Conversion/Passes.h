1f27201597baf9a7661dbe28ca283f63c027ea82
patrick-schultz <pschultz@broadinstitute.org>
2021-12-07 16:26:09 -0500

    Make stub Optional lowering pass

---
+++ include/Conversion/Passes.h
@@ -0,0 +1,14 @@
+#ifndef HAIL_CONVERSION_PASSES_H
+#define HAIL_CONVERSION_PASSES_H
+
+#include "Conversion/OptionalToStandard/OptionalToStandard.h"
+
+namespace hail {
+
+/// Generate the code for registering conversion passes.
+#define GEN_PASS_REGISTRATION
+#include "Conversion/Passes.h.inc"
+
+} // namespace hail
+
+#endif // HAIL_CONVERSION_PASSES_H
