9282f3a550bc217e237d56c1cc82fbe6ec91ad4b
patrick-schultz <pschultz@broadinstitute.org>
2021-12-09 15:06:07 -0500

    Push optional lowering through scf.if

--- lib/Conversion/OptionalToStandard/OptionalToStandard.cpp
+++ lib/Conversion/OptionalToStandard/OptionalToStandard.cpp
@@ -64,10 +64,86 @@ struct ConvertConsumeOptOp : public OpRewritePattern<ConsumeOptOp> {
   }
 };
 
+struct ConvertIfReturningOptional : public OpRewritePattern<scf::IfOp> {
+  using OpRewritePattern<scf::IfOp>::OpRewritePattern;
+
+  void transferBody(Block *source, Block *dest, ArrayRef<OpResult> optResults, PatternRewriter &rewriter) const {
+    Type i1Type = rewriter.getI1Type();
+    // Move all operations to the destination block.
+    rewriter.mergeBlocks(source, dest);
+    // Insert unpack operations and update the yielded results
+    auto yieldOp = cast<scf::YieldOp>(dest->getTerminator());
+    rewriter.setInsertionPoint(yieldOp);
+    rewriter.startRootUpdate(yieldOp);
+    for (auto en : llvm::enumerate(optResults)) {
+      auto i = en.index();
+      auto result = en.value();
+      unsigned int newIdx = result.getResultNumber() + i;
+      auto unpack = rewriter.create<UnpackOptionalOp>(yieldOp->getLoc(), i1Type,
+                                        result.getType().cast<OptionalType>().getValueType(),
+                                        yieldOp->getOperand(newIdx));
+      yieldOp->setOperands(newIdx, 1, unpack.getResults());
+    }
+    rewriter.finalizeRootUpdate(yieldOp);
+  }
+
+  LogicalResult matchAndRewrite(scf::IfOp ifOp,
+                                PatternRewriter &rewriter) const {
+    Type i1Type = rewriter.getI1Type();
+    // compute the set of optional results, and the new expanded result types
+    SmallVector<OpResult, 4> optResults;
+    SmallVector<Type, 4> newResultTypes;
+    for (auto result : ifOp.getResults()) {
+      Type resultType = result.getType();
+      if (auto optResultType = resultType.dyn_cast<OptionalType>()) {
+        Type valueType = optResultType.getValueType();
+        optResults.push_back(result);
+        newResultTypes.push_back(i1Type);
+        newResultTypes.push_back(valueType);
+      } else {
+        newResultTypes.push_back(resultType);
+      }
+    }
+
+    // Create a replacement operation with empty then and else regions.
+    auto emptyBuilder = [](OpBuilder &, Location) {};
+    auto newOp = rewriter.create<scf::IfOp>(ifOp.getLoc(), newResultTypes, ifOp.condition(),
+                                            /* withElseRegion = */ true);
+
+    // Move the bodies and update the terminators
+    transferBody(ifOp.getBody(0), newOp.getBody(0), optResults, rewriter);
+    transferBody(ifOp.getBody(1), newOp.getBody(1), optResults, rewriter);
+    rewriter.setInsertionPoint(ifOp);
+
+    // Add pack operations after the if, and compute the values to replace the old if
+    SmallVector<Value, 4> repResults;
+    for (auto en : llvm::enumerate(optResults)) {
+      auto i = en.index();
+      auto result = en.value();
+      unsigned int newIdx = result.getResultNumber() + i;
+      Type resultType = result.getType();
+      if (auto optResultType = resultType.dyn_cast<OptionalType>()) {
+        Type valueType = optResultType.getValueType();
+        auto pack = rewriter.create<PackOptionalOp>(ifOp->getLoc(), optResultType,
+                                                   newOp.getResult(newIdx), newOp.getResult(newIdx + 1));
+        repResults.push_back(pack.result());
+      } else {
+        repResults.push_back(result);
+      }
+    }
+
+    // Replace the operation by the new one.
+    rewriter.replaceOp(ifOp, repResults);
+
+    return success();
+  }
+};
+
 }
 
 void hail::populateOptionalToStdConversionPatterns(RewritePatternSet &patterns) {
-  patterns.add<ConvertPresentOp, ConvertMissingOp, ConvertConsumeOptOp>(patterns.getContext());
+  patterns.add<ConvertPresentOp, ConvertMissingOp, ConvertConsumeOptOp,
+               ConvertIfReturningOptional>(patterns.getContext());
 }
 
 void OptionalToStandardPass::runOnOperation() {
@@ -76,6 +152,12 @@ void OptionalToStandardPass::runOnOperation() {
   // Configure conversion to lower out .... Anything else is fine.
   ConversionTarget target(getContext());
   target.addIllegalOp<PresentOp, MissingOp, ConsumeOptOp>();
+  target.addDynamicallyLegalOp<scf::IfOp>([](scf::IfOp op) {
+    for (auto result : op.results()) {
+      if (result.getType().isa<OptionalType>()) return false;
+    }
+    return true;
+  });
   target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
   if (failed(
           applyPartialConversion(getOperation(), target, std::move(patterns))))
