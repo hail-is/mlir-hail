298aa13dc2f20cad21369e311f350b614c00fabc
patrick-schultz <pschultz@broadinstitute.org>
2021-12-09 10:35:37 -0500

    wip

--- include/Optional/OptionalDialect.h
+++ include/Optional/OptionalDialect.h
@@ -4,6 +4,7 @@
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
