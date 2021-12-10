1e2e2a885a7c23c7fb827e8f371a1c50b742270b
Patrick Schultz <pschultz@broadinstitute.org>
2021-12-10 14:28:12 -0500

    add verification to control dialect, add callcc op (#33)

--- examples/consume-co-opt.mlir
+++ examples/consume-co-opt.mlir
@@ -1,10 +1,13 @@
 func @consume_co_opt(%return: !control.continuation<i32>) -> i32 {
   %val = constant 3 : i32
 
-  %0 = optional.construct_co_opt {
-  ^bb0(%missing: !control.continuation<i32>, %present: !control.continuation<i32>):
-    control.apply_cont(%present, %val) : (!control.continuation<i32>, i32)
-  } : !optional.co_option<i32>
+  %result = control.callcc : (i32) {
+    %0 = optional.construct_co_opt {
+    ^bb0(%missing: !control.continuation<i32>, %present: !control.continuation<i32>):
+      control.apply(%present, %val) : (!control.continuation<i32>, i32)
+    } : !optional.co_option<i32>
 
-  optional.consume_co_opt(%0, %return, %return) : (!optional.co_option<i32>, !control.continuation<i32>, !control.continuation<i32>)
+    optional.consume_co_opt(%0, %return, %return) : (!optional.co_option<i32>, !control.continuation<i32>, !control.continuation<i32>)
+  }
+  return %result : i32
 }
--- lib/Control/ControlOps.cpp
+++ lib/Control/ControlOps.cpp
@@ -2,8 +2,48 @@
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/PatternMatch.h"
 
-#define GET_OP_CLASSES
-#include "Control/ControlOps.cpp.inc"
-
 using namespace mlir;
 using namespace hail::control;
+
+Block *DefContOp::body() { return &bodyRegion().back(); }
+
+static LogicalResult verify(DefContOp op) {
+  auto bodyTypes = op.body()->getArgumentTypes();
+  auto contArgTypes = op.getType().cast<ContinuationType>().getInputTypes();
+
+  if (contArgTypes != bodyTypes)
+    return op.emitOpError(
+      "expects the continuation body's arguments to have the same types "
+      "as the continuation type's parameters");
+
+  return success();
+}
+
+static LogicalResult verify(ApplyContOp op) {
+  auto contTypes = op.continuation().getType().cast<ContinuationType>().getInputTypes();
+  auto argTypes = op.values().getTypes();
+
+  if (contTypes != argTypes)
+    return op.emitOpError("mismatched continuation and argument types");
+
+  return success();
+}
+
+Block *CallCCOp::body() { return &bodyRegion().back(); }
+
+static LogicalResult verify(CallCCOp op) {
+  auto bodyTypes = op.body()->getArgumentTypes();
+  if (bodyTypes.size() != 1) return op.emitOpError("body must take a single continuation");
+  auto contType = bodyTypes[0].dyn_cast<ContinuationType>();
+  if (!contType) return op.emitOpError("body must take a single continuation");
+  auto contArgTypes = contType.getInputTypes();
+  auto resultTypes = op.getResultTypes();
+
+  if (resultTypes != contArgTypes)
+    return op.emitOpError("mismatched continuation and result types");
+
+  return success();
+}
+
+#define GET_OP_CLASSES
+#include "Control/ControlOps.cpp.inc"
